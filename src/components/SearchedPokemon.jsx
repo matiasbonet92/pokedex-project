62fafc5df7ac706d88300f7016a95581b2f9d09b
matiasbonet92 <you@example.commatias.g.bonet@gmail.com>
2022-09-22 21:23:32 -0300

    v04 - Searched upgrade

--- src/components/SearchedPokemon.jsx
+++ src/components/SearchedPokemon.jsx
@@ -28,36 +28,16 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-const SearchedPokemon = ({name}) => {
+const SearchedPokemon = ({pokemon}) => {
   
-    const [info,setInfo] = useState([]);
-    const [isPokemon, setIsPokemon] = useState(false);
-    const classes = useStyles();
-    var error = false;
+    const classes = useStyles();    
 
-    useEffect(() =>{
-        getSearchedPokemon(name);
-    },[name])
-
-    const getSearchedPokemon = async (name) => {
-        try{
-            const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-            const pokemonData = await data.json();
-            console.log(pokemonData);
-            setIsPokemon(true);
-            setInfo(pokemonData);
-        }catch (e) {
-            console.log(e);
-            error = true;
-        }
-    }
-
-    if(!isPokemon){
+    if(Object.keys(pokemon).length === 0){
         return(
             <Alert severity="error">The Pokemon doesn't exists! Please search again.</Alert>
         )
     }else{
-        return (
+        return(
             <Grid className={classes.main} item lg={2} md={3} sm={4} xs={6}>
                 <Card className={classes.card}>
                     <CardHeader
@@ -66,36 +46,24 @@ const SearchedPokemon = ({name}) => {
                                 <img src={imagen} alt="" height="30" width="30" />
                             </Avatar>
                         }
-                        title={info.name}
-                        subheader={info.order}
+                        title={pokemon?.name}
+                        subheader={pokemon?.order}
                         align="end"
                     />
                     <CardMedia
                         className={classes.cardMedia}
                         component="img"
                         height="200"
-                        alt={info.name}
-                        image={info?.sprites?.front_default}
+                        alt={pokemon?.name}
+                        image={pokemon?.sprites?.front_default}
                     />
-                    <CardContent>
-                        <Typography variant="h5" color="primary" align="center">
-                            Some Moves:
-                            {info?.moves?.filter((move, index) => index < 5).map((move) => {
-                                return(
-                                    <Typography variant="body2" color="text.secondary" align="justify">
-                                        {'- '+move.move.name}
-                                    </Typography>
-                                )
-                            })}
-                        </Typography>
-                    </CardContent>
                     <Divider />
                     <CardActions align="center">
                         <Chip icon={<CatchingPokemonIcon/>} label="Type" color="primary"/>
                         <div>
-                            {info?.types?.map((item) => {
+                            {pokemon?.types?.map((item) => {
                                 return(
-                                    <Typography variant="subtitle2" color="text.disabled">
+                                    <Typography key={pokemon?.id} variant="subtitle2" color="text.disabled">
                                         {item.type.name}
                                     </Typography>
                                 )
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { makeStyles } from '@mui/styles';
 import SearchedPokemon from '../components/SearchedPokemon'
+import CircularProgress from '@mui/material/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
   main:{
@@ -19,11 +20,27 @@ const Searched = () => {
 
   let params = useParams();
   const classes = useStyles();
+  const [pokemon,setPokemon] = useState({});
+  const [isLoading,setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPokemon();
+  },[pokemon])  
+  
+  const getPokemon = async () => {
+    setIsLoading(false);
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.search}`);
+    const pokemon = await data.json();
+    setPokemon(pokemon);
+  } 
+
+  if(isLoading) return(<CircularProgress />)
 
   return (
     <Container>
       <Grid className={classes.main} container>
-        <SearchedPokemon name={params.search}/>
+        <SearchedPokemon key={pokemon.id} pokemon={pokemon}/>
       </Grid>
     </Container>
   )
